21ff14d0fae28bd27bc7be907b168f66cf424439
pascudder <pascudder@wisc.edu>
2024-06-04 21:29:11 +0000

    streamlined contour plot script. Need to convert from pixels to m/s.

--- contours.py
+++ contours.py
@@ -2,6 +2,7 @@ from matplotlib import pyplot as plt
 import xarray as xr
 import numpy as np
 import cartopy.crs as ccrs
+import netCDF4 as nc
 
 uv_1 = xr.open_dataset('./data/uv_2016-06-01_00:00:00_P500_out.nc')
 uv_2 = xr.open_dataset('./data/uv_2016-06-01_03:00:00_P500_out.nc')
@@ -31,66 +32,22 @@ cb.ax.tick_params(labelsize=10)
 # there are too many u and v, we must splice
 # qv = plt.quiver(lon, lat, U2M_nans[0,:,:], V2M_nans[0,:,:], scale=350, color='k')
 
-# splice array:
+#splice array:
 qv = plt.quiver(lon[::60], lat[::40], u[::40, ::60], v[::40, ::60], color='k')
 plt.title('ECO1280')
 plt.tight_layout()
 plt.savefig('contour_quivers_eco.png')
 
-def load_windflow(file1, file2):
+with nc.Dataset('data.nc', 'r') as f:
+    lats = f.variables['lat'][:]
+    lons = f.variables['lon'][:]
+    gp_rad1 = f.variables['gp_rad1'][:]
+    gp_rad2 = f.variables['gp_rad2'][:]
+    u = f.variables['uwind'][:]
+    v = f.variables['vwind'][:]
 
-    # read windflow
-    print('reading windflow')
-    import torch
-    torch.device('cpu')
-    import numpy as np
-    from windflow import inference_flows
-    from windflow.datasets.daves_grids import Eco
-
-    checkpoint_file = 'model_weights/windflow.raft.pth.tar'
-    inference = inference_flows.FlowRunner('RAFT',
-                                        overlap=128,
-                                        tile_size=512,
-                                        device=torch.device('cpu'),
-                                        batch_size=1)
-    inference.load_checkpoint(checkpoint_file)
-
-    gp_1 = Eco(file1)
-    gp_ds1 = gp_1.open_dataset(scale_factor=25000, rescale=True)
-    gp_2 = Eco(file2)
-    gp_ds2 = gp_2.open_dataset(scale_factor=25000, rescale=True)
-
-    gp_rad1 = gp_ds1['gp_newP'] 
-    gp_rad2 = gp_ds2['gp_newP']
-    lat = gp_ds1['lat_0'].values
-    lon = gp_ds1['lon_0'].values
-
-    try:
-        shape = np.shape(gp_rad1)
-        gp_rad1 = gp_rad1.values.reshape((shape[0], shape[1])) #reshaped to (1801,3600)
-        gp_rad2 = gp_rad2.values.reshape((shape[0], shape[1])) #reshaped to (1801,3600)
-       
-    except ValueError as e:
-        print(e)
-        raise
-
-    # pass 2D humidity grid into windflow 
-    _, flows = inference.forward(gp_rad1, gp_rad2)
-
-    W_u = flows[0]
-    W_v = flows[1]
-
-    return W_u, -W_v, lat, lon
-
-file1 = './data/gp_2016-06-01_00:00:00_P500_out.nc'
-file2 = './data/gp_2016-06-01_03:00:00_P500_out.nc'
-
-u, v, lat, _ = load_windflow(file1, file2)
-
-# do calculations convert pixel to m/s
-expanded_lat = np.tile(lat, (3600, 1)).T #reshaped to (1801,3600)
-u = (u * 0.1 * 111 * 1000 * np.cos(expanded_lat)) / 10800
-v = (v * 0.1 * 111 * 1000) / 10800
+#flip v
+v = -v
 
 ws = (u**2 + v**2)**0.5
 max_ws = np.nanmax(ws)
