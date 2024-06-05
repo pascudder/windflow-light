Increased tile size, increased mask size, fixed(?) unit calculation to reduce bias

contours.py
@@ -24,7 +24,7 @@ assert np.all(lat == w_lat)
 #calculate windspeed and plot
 ws = (eco_u**2 + eco_v**2)**0.5
 max_ws = np.nanmax(ws)
-print("Maximum wind speed:", max_ws)
+print("Eco maximum wind speed:", max_ws)
 c_inv = np.arange(0, 80, 1)
 fig = plt.figure(figsize=(13,7))
 ax = plt.axes(projection=ccrs.PlateCarree())
@@ -44,14 +44,14 @@ plt.tight_layout()
 plt.savefig('contour_quivers_eco.png',dpi = 300)
 
 #convert from pixels to m/s
-expanded_lat = np.radians(np.tile(lat, (3600, 1)).T)
-w_u = (w_u * 0.1 * 111 * 1000 * np.cos(expanded_lat)) / 10800 #EXPANDED_LAT IS IN RADIANS
-w_v = (w_v * 0.1 * 111 * 1000) / 10800
+expanded_lat = np.radians(np.tile(lat, (3600, 1)).T) #EXPANDED LAT MUST BE IN RADIANS
+w_u = (w_u * 0.1 * 111 * 1000 * np.cos(expanded_lat)) / 10800
+w_v = (w_v * 0.05 * 111 * 1000) / 10800
 
 #calculate windspeed and plot
 ws = (w_u**2 + w_v**2)**0.5
 max_ws = np.nanmax(ws)
-print("Maximum wind speed:", max_ws)
+print("Windflow maximum wind speed:", max_ws)
 c_inv = np.arange(0, 80, 1)
 fig = plt.figure(figsize=(13,7))
 ax = plt.axes(projection=ccrs.PlateCarree())

humidity.py
@@ -26,7 +26,7 @@ assert np.all(lat == w_lat)
 #convert from pixels to m/s
 expanded_lat = np.radians(np.tile(lat, (3600, 1)).T)
 w_u = (w_u * 0.1 * 111 * 1000 * np.cos(expanded_lat)) / 10800 #EXPANDED_LAT IS IN RADIANS
-w_v = (w_v * 0.1 * 111 * 1000) / 10800
+w_v = (w_v * 0.05 * 111 * 1000) / 10800
 
 #plot
 projection = ccrs.PlateCarree()
@@ -34,7 +34,7 @@ projection = ccrs.PlateCarree()
 print('plotting')
 # ECO 1280 time 1
 fig, ax = plt.subplots(subplot_kw={'projection': projection}, figsize=(12, 7))
-im = ax.pcolormesh(lon, lat, gp_rad1, transform=projection, cmap='Blues_r')
+im = ax.pcolormesh(lon, lat, gp_rad1, transform=projection, cmap=plt.cm.jet)
 qv = plt.quiver(lon[::60], lat[::40], eco_u[::40, ::60], eco_v[::40, ::60], color='black', width=0.0015)
 cbar = plt.colorbar(im, ax=ax, shrink=0.7, label='Humidity kg kg-1')
 ax.coastlines(color='white')
@@ -45,19 +45,19 @@ plt.close()
 
 # ECO1280 time 2
 fig, ax = plt.subplots(subplot_kw={'projection': projection}, figsize=(12, 7))
-im = ax.pcolormesh(lon, lat, gp_rad2, transform=projection, cmap='Blues_r')
+im = ax.pcolormesh(lon, lat, gp_rad2, transform=projection, cmap=plt.cm.jet)
 qv = plt.quiver(lon[::60], lat[::40], eco_u[::40, ::60], eco_v[::40, ::60], color='black', width=0.0015)
 cbar = plt.colorbar(im, ax=ax, shrink = 0.7, label='Humidity kg kg-1')
 plt.title('ECO1280 Humidity - Snapshot 2')
 ax.coastlines(color='white')
 plt.tight_layout()
-#plt.savefig('humidity_quivers_eco2.png', dpi=300)
+plt.savefig('humidity_quivers_eco2.png', dpi=300)
 plt.close()
 
 
 # windflow time 1
 fig, ax = plt.subplots(subplot_kw={'projection': projection}, figsize=(12, 7))
-im = ax.pcolormesh(lon, lat, gp_rad1, transform=projection, cmap='Blues_r')
+im = ax.pcolormesh(lon, lat, gp_rad1, transform=projection, cmap=plt.cm.jet)
 qv = plt.quiver(lon[::60], lat[::40], w_u[::40, ::60], w_v[::40, ::60], color='black', width=0.0015)
 cbar = plt.colorbar(im, shrink=0.7, ax=ax, label='Humidity kg kg-1')
 plt.title('Windflow Humidity - Snapshot 1')
@@ -68,36 +68,12 @@ plt.close()
 
 # windflow time 2
 fig, ax = plt.subplots(subplot_kw={'projection': projection}, figsize=(12, 7))
-im = ax.pcolormesh(lon, lat, gp_rad2, transform=projection, cmap='Blues_r')
+im = ax.pcolormesh(lon, lat, gp_rad2, transform=projection, cmap=plt.cm.jet)
 qv = plt.quiver(lon[::60], lat[::40], w_u[::40, ::60], w_v[::40, ::60], color='black', width=0.0015)
 cbar = plt.colorbar(im, ax=ax, shrink = 0.7, label='Humidity kg kg-1')
 plt.title('Windflow Humidity - Snapshot 2')
 ax.coastlines(color='white')
 plt.tight_layout()
-#plt.savefig('humidity_quivers_windflow2.png', dpi=300)
+plt.savefig('humidity_quivers_windflow2.png', dpi=300)
 plt.close()
 
-
-'''#barbs
-fig, ax = plt.subplots(subplot_kw={'projection': projection}, figsize=(12, 7))
-im = ax.pcolormesh(lon, lat, gp_rad1, transform=projection, cmap='Blues_r')
-bv = ax.barbs(lon[::100], lat[::80], W_u[::80, ::100], -W_v[::80, ::100], color='black', length=4, linewidth=0.8)
-cbar = plt.colorbar(im, ax=ax, label='Humidity kg kg-1')
-plt.title('Windflow Humidity')
-ax.coastlines(color='white')
-plt.tight_layout()
-plt.savefig('humidity_barbs_windflow1_blues.png', dpi=300)
-# plt.show()
-plt.close()
-
-fig, ax = plt.subplots(subplot_kw={'projection': projection}, figsize=(12, 7))
-im = ax.pcolormesh(lon, lat, gp_rad2, transform=projection, cmap='Blues_r')
-bv = ax.barbs(lon[::100], lat[::80], W_u[::80, ::100], -W_v[::80, ::100], color='black', length=4, linewidth=0.8)
-cbar = plt.colorbar(im, ax=ax, label='Humidity kg kg-1')
-plt.title('Windflow Humidity')
-ax.coastlines(color='white')
-plt.tight_layout()
-plt.savefig('humidity_barbs_windflow2_blues.png', dpi=300)
-# plt.show()
-plt.close()
-'''

run_windflow.py
@@ -10,7 +10,7 @@ import netCDF4 as nc
 checkpoint_file = 'model_weights/windflow.raft.pth.tar'
 inference = inference_flows.FlowRunner('RAFT',
                                      overlap=128,
-                                     tile_size=512,
+                                     tile_size=1024,
                                      device=torch.device('cpu'),
                                      batch_size=1)
 inference.load_checkpoint(checkpoint_file)

scatter.py
@@ -28,8 +28,8 @@ def main():
     assert np.all(lat == w_lat)
 
     expanded_lat = np.tile(lat, (3600, 1)).T #reshaped to (1801,3600)
-    mask = (expanded_lat <= 30) & (expanded_lat >= -30) # mask super northern and southern regions
-                                                        #This range seems small - consider working with higher values?
+    mask = (expanded_lat <= 90) & (expanded_lat >= -90) # mask the region of interest
+    
     lat_mask = np.radians(expanded_lat[mask])  # convert latitude from degrees to radians
 
     #select masked regions
@@ -40,38 +40,37 @@ def main():
     wv_mask = w_v[mask]
 
     # do calculations convert pixel to m/s
-    wu_mask = (wu_mask * 0.1 * 111 * 1000 * np.cos(lat_mask)) / 10800 #LAT_MASK IS IN RADIANS
-    wv_mask = (wv_mask * 0.1 * 111 * 1000) / 10800
-
+    wu_mask = (wu_mask * 0.1 * 111 * 1000) * np.cos(lat_mask) / 10800 #LAT_MASK IS IN RADIANS
+    wv_mask = (wv_mask * 0.05 * 111 * 1000) / 10800 #test to reduce bias
     # calculate MSE of the u component
     x = eu_mask
     y = wu_mask
-    
+
     print(f'MSE: u: {np.nanmean((y-x)**2)}')
 
     #U component plots
     ax = scatter(x, y, s=1, textbox=(-18, 55))
     ax.set_xlim(-20, 40)
     ax.set_ylim(-20, 40)
-    ax.set_title("2016-06-01 00:00:00 P500 U component -30 to 30 lat")
+    ax.set_title("2016-06-01 00:00:00 P500 U component -90 to 90 lat")
     ax.set_xlabel('ECO1280 u-component m/s')
     ax.set_ylabel('Windflow u-component m/s')
-    #plt.savefig("scatter.ucomp_500_90to90_pixel.png")
+    plt.savefig("scatter.ucomp_500_90to90_pixel.png")
 
     ax = density_scatter(x, y, s=1, bins=150)
     ax.set_xlim(-20, 40)
     ax.set_ylim(-20, 40)
-    ax.set_title("2016-06-01 00:00:00 P500 U component density -30 to 30 lat")
+    ax.set_title("2016-06-01 00:00:00 P500 U component density -90 to 90 lat")
     ax.set_xlabel('ECO1280 u-component m/s')
     ax.set_ylabel('Windflow u-component m/s')
-    #plt.savefig("scatter_density.ucomp_500_30to30_pixel.png")
+    plt.savefig("scatter_density.ucomp_500_90to90_pixel.png")
 
     udiff = (y - x)
     ax = scatter(udiff, expanded_lat[mask], axline=False)
     ax.set_title("2016-06-01 00:00:00 P500 U diff (windflow - eco) vs lat")
     ax.set_xlabel('Udiff u-component (windflow - eco) m/s')
     ax.set_ylabel('Latitude')
-    #plt.savefig("scatter_lat.ucomp_500_90to90_pixel.png")
+    plt.savefig("scatter_lat.ucomp_500_90to90_pixel.png")
 
     #calculate MSE of v component
     x = ev_mask
@@ -83,25 +82,25 @@ def main():
     ax = scatter(x, y, s=1, textbox=(-18, 35))
     #ax.set_xlim(-40, 40)
     #ax.set_ylim(-40, 40)
-    ax.set_title("2016-06-01 00:00:00 P500 V component 30 to 30 lat")
+    ax.set_title("2016-06-01 00:00:00 P500 V component 90 to 90 lat")
     ax.set_xlabel('ECO1280 v-component m/s')
     ax.set_ylabel('Windflow v-component m/s')
-    #plt.savefig("scatter.vcomp_500_60to90_pixel.png")
+    plt.savefig("scatter.vcomp_500_90to90_pixel.png")
 
     ax = density_scatter(x, y, s=1, bins=150)
     ax.set_xlim(-20, 40)
     ax.set_ylim(-20, 40)
-    ax.set_title("2016-06-01 00:00:00 P500 V component density -30 to 30 lat")
+    ax.set_title("2016-06-01 00:00:00 P500 V component density -90 to 90 lat")
     ax.set_xlabel('ECO1280 v-component m/s')
     ax.set_ylabel('Windflow v-component m/s')
-    #plt.savefig("scatter_density.vcomp_500_30to30_pixel.png")
+    plt.savefig("scatter_density.vcomp_500_90to90_pixel.png")
 
     vdiff = (y - x)
     ax = scatter(vdiff, expanded_lat[mask], axline=False)
     ax.set_title("2016-06-01 00:00:00 P500 V diff (windflow - eco) vs lat")
     ax.set_xlabel('Vdiff v-component (windflow - eco) m/s')
     ax.set_ylabel('Latitude')
-    #plt.savefig("scatter_lat.vcomp_500_90to90_pixel.png")
+    plt.savefig("scatter_lat.vcomp_500_90to90_pixel.png")
 
 def scatter(x, y, s=1, textbox=(0, 0), axline=True):
     fig, ax = plt.subplots(figsize=(8,8))
@@ -113,9 +112,9 @@ def scatter(x, y, s=1, textbox=(0, 0), axline=True):
     xmin, xmax = ax.get_xlim()
     ymin, ymax = ax.get_ylim()
 
-    props = dict(boxstyle='round', facecolor='aquamarine', alpha=0.5)
-    textstr = f'Stats: \nSample Number: {num_samples} \nBias: {bias:.3f}\nRMSE: {rmse:.3f}'
-    ax.text(xmin + 2, ymax - 2, textstr, fontsize=10, verticalalignment='top', bbox=props)
+    textstr = f'Bias: {bias:.3f}\nRMSE: {rmse:.3f}'
+    props = dict(boxstyle='round', facecolor='white', alpha=0.8)
+    ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=12, verticalalignment='top', bbox=props)
     
     if (axline):
         ax.axline((0, 0), slope=1, color='black', lw=1)
@@ -143,6 +142,13 @@ def density_scatter(x , y, s=1, axline=True, sort=True, bins=20, **kwargs )   :
     ax.scatter( x, y, c=z, s=s, **kwargs )
     if (axline):
         ax.axline((0, 0), slope=1, color='black', lw=1)
+
+    bias = np.nanmean(y - x)
+    rmse = np.sqrt(np.nanmean((y - x)**2))
+
+    textstr = f'Bias: {bias:.3f}\nRMSE: {rmse:.3f}'
+    props = dict(boxstyle='round', facecolor='white', alpha=0.8)
+    ax.text(0.05, 0.95, textstr, transform=ax.transAxes, fontsize=12, verticalalignment='top', bbox=props)
 
     norm = Normalize(vmin = np.min(z), vmax = np.max(z))
     cbar = fig.colorbar(cm.ScalarMappable(norm = norm), ax=ax)