fed0a3b06d21bd565f18806b3a49462830bd70e7
pascudder <pascudder@wisc.edu>
2024-06-12 14:24:37 +0000

    improved plots

--- humidity.py
+++ humidity.py
@@ -3,6 +3,8 @@ import numpy as np
 import netCDF4 as nc
 from eco1280_loader import load_eco1280
 import cartopy.crs as ccrs
+from PIL import Image
+
 
 # Load ECO1280 data
 file1 = './data/uv_2016-06-01_00:00:00_P500_out.nc' 
@@ -20,15 +22,11 @@ with nc.Dataset('data.nc', 'r') as f:
     w_v = f.variables['vwind'][:]
 assert np.all(lat == w_lat)
 
-# Plot
+# Create and save the first plot
 projection = ccrs.PlateCarree()
+fig, ax = plt.subplots(subplot_kw={'projection': projection}, figsize=(10, 6))
 
-print('plotting')
-fig, axes = plt.subplots(2,1, subplot_kw={'projection': projection}, figsize=(20, 12))
-
-# Combined plot time 1
-ax = axes[0]
-im = ax.pcolormesh(lon, lat, gp_rad1, transform=projection, cmap=plt.cm.jet,vmax=140)
+im = ax.pcolormesh(lon, lat, gp_rad1, transform=projection, cmap=plt.cm.jet, vmax=140)
 qv_eco = ax.quiver(lon[::60], lat[::40], eco_u[::40, ::60], eco_v[::40, ::60], color='black', width=0.0015, label='ECO1280')
 qv_wind = ax.quiver(lon[::60], lat[::40], w_u[::40, ::60].data, w_v[::40, ::60].data, color='red', width=0.0012, label='Windflow')
 cbar = plt.colorbar(im, ax=ax, shrink=0.5, label='Humidity kg kg-1')
@@ -36,9 +34,14 @@ ax.coastlines(color='white')
 ax.set_title('Combined Quivers - Snapshot 1')
 ax.legend(loc='upper right', bbox_to_anchor=(1.1, 1.0))
 
-# Combined plot time 2
-ax = axes[1]
-im = ax.pcolormesh(lon, lat, gp_rad2, transform=projection, cmap=plt.cm.jet,vmax=140)
+plt.tight_layout()
+plt.savefig('snapshot1.png', bbox_inches='tight', dpi=300)
+plt.close()
+
+# Create and save the second plot
+fig, ax = plt.subplots(subplot_kw={'projection': projection}, figsize=(10, 6))
+
+im = ax.pcolormesh(lon, lat, gp_rad2, transform=projection, cmap=plt.cm.jet, vmax=140)
 qv_eco = ax.quiver(lon[::60], lat[::40], eco_u[::40, ::60], eco_v[::40, ::60], color='black', width=0.0015, label='ECO1280')
 qv_wind = ax.quiver(lon[::60], lat[::40], w_u[::40, ::60], w_v[::40, ::60], color='red', width=0.0012, label='Windflow')
 cbar = plt.colorbar(im, ax=ax, shrink=0.5, label='Humidity kg kg-1')
@@ -47,5 +50,12 @@ ax.set_title('Combined Quivers - Snapshot 2')
 ax.legend(loc='upper right', bbox_to_anchor=(1.1, 1.0))
 
 plt.tight_layout()
-plt.savefig('combined_humidity_quivers.png', bbox_inches='tight', dpi=300)
-plt.show()
+plt.savefig('snapshot2.png', bbox_inches='tight', dpi=300)
+plt.close()
+
+# Combine the images into a GIF
+images = []
+for filename in ['snapshot1.png', 'snapshot2.png']:
+    images.append(Image.open(filename))
+images[0].save('combined_humidity_quivers.gif',
+               save_all=True, append_images=images[1:], duration=1000, loop=0)
